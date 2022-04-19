6d7d69c485ef4d05e84abcdb0e56acd4a054b9c2
Felix Castro <felixgonzalocastro@gmail.com>
2022-04-19 10:16:29 -0500

    ADD se edito la tabla con scroll y todas las colummnas de bd renueva

--- resources/views/admin/importfile/index.blade.php
+++ resources/views/admin/importfile/index.blade.php
@@ -41,26 +41,71 @@
         </div>
 
         @if ($data->count())
-            <div class="card-body">
-                <table class="table table-striped">
-                    <thead>
-                        <tr>
-                            <th scope="col">ID</th>
-                            <th scope="col">Nombre</th>
-                            <th scope="col">Email</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @foreach ($data as $item)
+            <div class="card-body"  style="overflow: hidden">
+                <div style="overflow: auto">
+                    <table class="table table-striped">
+                        <thead>
                             <tr>
-                                <td>{{ $item->sec }}</td>
-                                <td>{{ $item->cliente }}</td>
-                                <td>{{ $item->dni_ruc }}</td>
+                                <th scope="col">sec</th>
+                                <th scope="col">expediente</th>
+                                <th scope="col">codigo_cliente</th>
+                                <th scope="col">cod_bscs</th>
+                                <th scope="col">cliente</th>
+                                <th scope="col">dni_ruc</th>
+                                <th scope="col">linea</th>
+                                <th scope="col">plazo_acuerdo</th>
+                                <th scope="col">pdv</th>
+                                <th scope="col">cod_contrato</th>
+                                <th scope="col">fecha_renovacion</th>
+                                <th scope="col">liquidacion</th>
+                                <th scope="col">plan</th>
+                                <th scope="col">aaservicio</th>
+                                <th scope="col">estado_servicio</th>
+                                <th scope="col">cf</th>
+                                <th scope="col">factor_renovacion</th>
+                                <th scope="col">semana_pago</th>
+                                <th scope="col">fecha_pago</th>
+                                <th scope="col">extorno_topes</th>
+                                <th scope="col">extorno_sivco</th>
+                                <th scope="col">comision</th>
+                                <th scope="col">oc</th>
+                                <th scope="col">nota</th>
+                                <th scope="col">segmento</th>
                             </tr>
-                        @endforeach
-                    </tbody>
-                </table>
-
+                        </thead>
+                        <tbody>
+                            @foreach ($data as $item)
+                                <tr>
+                                    <td>{{ $item->sec }}</td>
+                                    <td>{{ $item->expediente }}</td>
+                                    <td>{{ $item->codigo_cliente }}</td>
+                                    <td>{{ $item->cod_bscs }}</td>
+                                    <td>{{ $item->cliente }}</td>
+                                    <td>{{ $item->dni_ruc }}</td>
+                                    <td>{{ $item->linea }}</td>
+                                    <td>{{ $item->plazo_acuerdo }}</td>
+                                    <td>{{ $item->pdv }}</td>
+                                    <td>{{ $item->cod_contrato }}</td>
+                                    <td>{{ $item->fecha_renovacion }}</td>
+                                    <td>{{ $item->liquidacion }}</td>
+                                    <td>{{ $item->plan }}</td>
+                                    <td>{{ $item->aaservicio }}</td>
+                                    <td>{{ $item->estado_servicio }}</td>
+                                    <td>{{ $item->cf }}</td>
+                                    <td>{{ $item->factor_renovacion }}</td>
+                                    <td>{{ $item->semana_pago }}</td>
+                                    <td>{{ $item->fecha_pago }}</td>
+                                    <td>{{ $item->extorno_topes }}</td>
+                                    <td>{{ $item->extorno_sivco }}</td>
+                                    <td>{{ $item->comision }}</td>
+                                    <td>{{ $item->oc }}</td>
+                                    <td>{{ $item->nota }}</td>
+                                    <td>{{ $item->segmento }}</td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
             </div>
             <div class="card-footer">
                 {{ $data->links() }}
