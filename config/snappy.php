b78e5ae305efaff63953f82f5313397b99b39d19
Yaseito <borjalipatrick792@gmail.com>
2022-05-24 11:56:53 -0500

    database/migrations/2022_05_03_002940_create_bsd_venta_table.php

--- config/snappy.php
+++ config/snappy.php
@@ -36,7 +36,7 @@ return [
     'pdf' => [
         'enabled' => true,
         // 'binary'  => env('WKHTML_PDF_BINARY', '/usr/local/bin/wkhtmltopdf'),
-        'binary' => base_path('vendor/h4cc/wkhtmltopdf-amd64/bin/wkhtmltopdf-amd64'),
+        'binary' => base_path('vendor\wemersonjanuario\wkhtmltopdf-windows\bin\64bit\wkhtmltopdf'),
         'timeout' => false,
         'options' => [],
         'env'     => [],
