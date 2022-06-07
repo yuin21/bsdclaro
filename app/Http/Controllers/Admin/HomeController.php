d3690ce4d52e43359e5ab581df8caab07f42cc4d
Felix Castro <felixgonzalocastro@gmail.com>
2022-06-07 14:20:26 -0500

    ADD plantilla de graficos en dashboard

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -4,10 +4,14 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\BsdVenta;
 
 class HomeController extends Controller
 {
     public function index(){
+        $year = date("Y");
+        $lastYear = $year - 1;
+        // $ventas = BsdVenta::get();
         return view('admin.index');
     }
 }
