063638240b25a506ebd4f96e94feb8b316fb320a
unknown <juniorferxd@gmail.com>
2022-06-08 12:49:12 -0500

    migraciones de las tablas pago y detalle pago

---
+++ database/migrations/2022_06_08_025136_create_bsd_pago_table.php
@@ -0,0 +1,52 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bsd_pago', function (Blueprint $table) {
+            $table->id();
+
+            $table->bigInteger('bsd_cuota_personal_id')->unsigned();
+            $table->foreign('bsd_cuota_personal_id')
+            ->references("id")
+            ->on("bsd_cuota_personal");
+
+            $table->bigInteger('bsd_venta_id')->unsigned();
+            $table->foreign('bsd_venta_id')
+            ->references("id")
+            ->on("bsd_venta");
+
+            
+            $table->float('porcentaje_comision');
+            $table->float('comision_consultor');
+            $table->char('estado_carpeta', 1)->comment('C or N'); 
+            $table->char('pago_1er_recibo', 1)->comment('S or N');
+            $table->char('pago_dace', 1)->comment('S or N');
+            $table->char('abono_consultor', 1)->comment('S or N');
+            $table->char('estado', 1)->default('1')->comment('1 or 0');
+            $table->string('usuario_reg', 255)->default('system');
+            $table->string('usuario_act', 255)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bsd_pago');
+    }
+};
---
+++ database/migrations/2022_06_08_025417_create_bsd_detalle_pago_table.php
@@ -0,0 +1,72 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bsd_detalle_pago', function (Blueprint $table) {
+            $table->id();
+
+            $table->bigInteger('bsd_pago_id')->unsigned();
+            $table->foreign('bsd_pago_id')
+            ->references("id")
+            ->on("bsd_pago");
+
+            $table->bigInteger('bsd_venta_id')->unsigned();
+            $table->foreign('bsd_venta_id')
+            ->references("id")
+            ->on("bsd_venta");
+
+            $table->bigInteger('bsd_detalle_venta_id')->unsigned();
+            $table->foreign('bsd_detalle_venta_id')
+            ->references("id")
+            ->on("bsd_detalle_venta");
+
+            $table->bigInteger('bsd_tipo_servicio_id')->unsigned();
+            $table->foreign('bsd_tipo_servicio_id')
+            ->references("id")
+            ->on("bsd_tipo_servicio");
+
+            $table->bigInteger('bsd_servicio_id')->unsigned();
+            $table->foreign('bsd_servicio_id')
+            ->references("id")
+            ->on("bsd_servicio");
+
+            $table->bigInteger('bsd_plan_id')->unsigned();
+            $table->foreign('bsd_plan_id')
+            ->references("id")
+            ->on("bsd_plan");
+
+            $table->float('cuota');
+            $table->float('comision_consultor');
+            $table->float('sub_total');
+            $table->float('porcentaje_cump_dic');
+            $table->float('sum_total_ventas');
+            $table->float('sum_renta_total_ventas');
+            $table->float('sum_comision_bruta_dace');
+            $table->char('estado', 1)->default('1')->comment('1 or 0');
+            $table->string('usuario_reg', 255)->default('system');
+            $table->string('usuario_act', 255)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bsd_detalle_pago');
+    }
+};
