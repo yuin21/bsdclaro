b6d9de60d1b376603e7412b63e426290b1a6cff7
Felix Castro <felixgonzalocastro@gmail.com>
2022-06-07 21:03:47 -0500

    ADD reportes de ventas diarias
    - filtros de fecha de registro y estado
    - generar pdf de los resultados
    ADICIONAL
    - FIX graficos de dashboard con ventas que tienen estado 1

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -12,11 +12,13 @@ class HomeController extends Controller
         $year = date("Y");
         $lastYear = $year - 1;
         //$ventas_currentYear = BsdVenta::select('id', 'total', 'fecha_registro')->whereYear('fecha_registro', $year)->get();
-        $ventas_currentYear = BsdVenta::select('bsd_venta.id', 'total', 'fecha_registro', 'bsd_personal_id', 'nom_personal', 'ape_paterno', 'ape_materno')
+        $ventas_currentYear = BsdVenta::select('bsd_venta.id', 'total','bsd_venta.estado',  'fecha_registro', 'bsd_personal_id', 'nom_personal', 'ape_paterno', 'ape_materno')
             ->join('bsd_personal', 'bsd_venta.bsd_personal_id', '=', 'bsd_personal.id')
+            ->where('bsd_venta.estado', 1)
             ->whereYear('fecha_registro', $year)->get();
-        $ventas_lastYear = BsdVenta::select('bsd_venta.id', 'total', 'fecha_registro', 'bsd_personal_id', 'nom_personal', 'ape_paterno', 'ape_materno')
+        $ventas_lastYear = BsdVenta::select('bsd_venta.id', 'total','bsd_venta.estado', 'fecha_registro', 'bsd_personal_id', 'nom_personal', 'ape_paterno', 'ape_materno')
             ->join('bsd_personal', 'bsd_venta.bsd_personal_id', '=', 'bsd_personal.id')
+            ->where('bsd_venta.estado', 1)
             ->whereYear('fecha_registro', $lastYear)->get();
         
         return view('admin.index', compact('year', 'lastYear', 'ventas_currentYear', 'ventas_lastYear'));
